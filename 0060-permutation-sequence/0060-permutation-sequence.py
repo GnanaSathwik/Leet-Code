81685a03e04fb2d53c23cc78afc974942c516a0d
GnanaSathwik <gnanasathwikgnv@gmail.com>
2026-06-10 20:48:01 +0530

    Time: 0 ms (100%), Space: 12.4 MB (58.44%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,18 +1,10 @@
 class Solution(object):
     def getPermutation(self, n, k):
-        lst = [x for x in range(1,n+1)]
-        fact = [1] * 10
-        for i in range(1, 10):
-            fact[i] = fact[i - 1] * i
-            
+        lst = [x for x in range(1,n+1)]     
         result = ""   
         while len(lst) != 0 :
-            count = 0
-            x = fact[n-1]
-            num = x
-            while k > num :
-                count += 1
-                num += x
+            x = factorial(n-1)
+            count = (k-1)//x
             result += str(lst[count])
             n -= 1
             k -= count*x
