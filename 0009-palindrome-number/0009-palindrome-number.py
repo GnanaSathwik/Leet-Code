10abc301daac517bbbcccadd71236f652d7bdb1e
GnanaSathwik <gnanasathwikgnv@gmail.com>
2026-06-10 17:14:48 +0530

    Time: 4 ms (90.34%), Space: 12.4 MB (54.51%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,17 +2,21 @@ class Solution(object):
     def isPalindrome(self, x):
         if x < 0:
             return False
+        
+        if x % 10 == 0 and x != 0:
+            return False
 
         
         num = x
         rev = 0
-        while num != 0:
+        while num > rev:
             rev = rev * 10 + (num % 10)
             num = num // 10
-        
-        
-        return rev == x
 
+        
+        return num == rev or  num == rev //10 
+        
+        
         """
         :type x: int
         :rtype: bool
